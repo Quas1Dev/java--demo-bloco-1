59a66aff4c53a877f8bbe8c8943600369355d59e
Fernando Bonfim <fernando.j.bonfim@gmail.com>
2024-06-18 22:35:01 -0300

    Initial files.

---
+++ src/blocodemo1/BlocoDemo1.java
@@ -0,0 +1,11 @@
+package blocodemo1;
+
+public class BlocoDemo1 {
+
+    // Instruções que fazem parte da classe.
+    public static void main(String[] args) {
+        // Instruções que fazem parte do método main.
+        System.out.println("Faço parte do método main!!!");
+    }
+
+}
